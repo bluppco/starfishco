7c9d52e541e6db26a352520c98555b9045eddfd3
Mohit <mohit@hey.com>
2024-01-17 22:42:13 +0530

    x

--- src/constants/cdn.js
+++ src/constants/cdn.js
@@ -1,2 +1,10 @@
 export const HOME_INTRO_VIDEO = "https://www.starfishco.com/wp-content/uploads/2022/12/starfish_desktop_18_sec-1080p.mp4"
 export const BRAND_EXPERIENCE_GIF = "https://www.starfishco.com/wp-content/themes/Starfish/assets/img/sf_textanimation_desktop.gif"
+export const CDN_LINK = "https://blupp.b-cdn.net/starfishco/"
+export const IMAGE_QUALITY = 80
+export const IMAGE_QUALITY_60 = 60
+export const IMAGE_WIDTH_600 = 600
+export const IMAGE_WIDTH = 800
+export const IMAGE_MOBILE_WIDTH = 600
+export const IMAGE_WIDTH_1400 = 1400
+export const IMAGE_DESKTOP_WIDTH = 1600
