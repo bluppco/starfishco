e89f9ab85f5d2a4909f3d956fbf9965787b109ed
Kunal Singh <kunalsin9h@gmail.com>
2024-01-20 20:25:25 +0530

    using local path for og image and favicon

--- src/constants/website.js
+++ src/constants/website.js
@@ -1,3 +1,3 @@
 export const WEBSITE = "https://starfishco.blupp.co/"
-export const OG_IMAGE = "https://www.starfishco.com/wp-content/uploads/2022/09/logo.svg"	
-export const FAVICON = "https://www.starfishco.com/wp-content/uploads/2022/11/logo-1.png"
+export const OG_IMAGE = "/og.svg"	
+export const FAVICON = "/favicon.webp"
