f4f28f1f29c9befebafa256f252405da9506f1ff
Mohit <mohit@hey.com>
2024-02-27 19:32:22 +0530

    x

--- src/api/home/index.js
+++ src/api/home/index.js
@@ -2,12 +2,22 @@ import { endpointFetch } from "../../functions/endpoint"
 
 export const homeAPI = async ( environment ) => {
 
-    const endpoint = endpointFetch( environment )
+    try {
 
-    // NETWORK CALL TO GET DATA
-    const network_data = await fetch( endpoint + "/home" )
-    const data_json = await network_data.json()
+        const endpoint = endpointFetch( environment )
 
-    return data_json.data
+        // NETWORK CALL TO GET DATA
+        const network_data = await fetch( endpoint + "/home" )
+        const data_json = await network_data.json()
+
+        return data_json.data
+
+    } catch ( error ){
+
+        console.log("error is here")
+        console.log( error )
+
+    }
+
 
 }
