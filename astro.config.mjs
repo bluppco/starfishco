5131839a0c0912ed9da4449bb2c8ddc6e0d3e47e
Mohit <mohit@hey.com>
2024-02-27 18:32:22 +0530

    x

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,10 +10,5 @@ import icon from "astro-icon";
 export default defineConfig({
   integrations: [tailwind(), react(), mdx(), icon()],
   output: "server",
-  adapter: cloudflare({
-    runtime: {
-      mode: 'local',
-      type: 'pages'
-    }
-  })
+  adapter: cloudflare()
 });
